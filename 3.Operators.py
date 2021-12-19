7dca09421f99185ae197afde6b1ea04c9c8ffb92
Aakash <aksverma26@gmail.com>
2021-12-19 17:16:26 +0530

    DAY 2

---
+++ 3.Operators.py
@@ -0,0 +1,37 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'solve' function below.
+#
+# The function accepts following parameters:
+#  1. DOUBLE meal_cost
+#  2. INTEGER tip_percent
+#  3. INTEGER tax_percent
+#
+
+def solve(meal_cost, tip_percent, tax_percent):
+    # Write your code here
+    cost1 = (tip_percent*meal_cost)/100
+    cost2 = (tax_percent*meal_cost)/100
+
+    cost = meal_cost + cost1 + cost2;
+
+    if ((int(cost)+0.5) <= cost):
+        print(math.ceil(cost))
+    else:
+        print(math.floor(cost))
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
